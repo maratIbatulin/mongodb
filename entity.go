0c2d3e8610f8ae0f934ce6738c452cd819d0bd15
Марат Ибатулин <progr10@variag.ru>
2024-07-16 17:21:06 +0500

    mongo.A

--- entity.go
+++ entity.go
@@ -1,10 +1,11 @@
 package mongo
 
 import (
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type D primitive.D
 
-type M bson.M
+type M primitive.M
+
+type A primitive.A
